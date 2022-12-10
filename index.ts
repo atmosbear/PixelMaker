6765e192e851f1ae9d3e0223b2d736f7a714dc28
atmosbear <mondegreen84@protonmail.com>
2022-12-10 10:24:35 -0800

    Added tool buttons!

--- index.ts
+++ index.ts
@@ -1,68 +1,160 @@
 import { Rectangle, Scene } from "pencil.js"
 
+/**
+ * Returns an element - assumes you know what you're calling exists!
+ */
 function el(id: string) {
     return document.getElementById(id)!
 }
 
+/**
+ * What the user sees as a single pixel in the drawing board. "Rectangle" is a Pencil.js object, while
+ * x and y are the pixel's coordinates.
+ */
+type Cell = {
+    rectangle: Rectangle, x: number, y: number
+}
+
+function getCellByCoords(x: number, y: number): Cell | undefined {
+    let ret
+    cells.forEach((cell) => {
+        if (cell.x === x && cell.y === y) {
+            ret = cell
+        }
+    })
+    return ret
+}
+
+function findNeighborsByDistanceOf(centerCell: Cell, maxDistance: number): Cell[] {
+    let neighbors: Cell[] = []
+    cells.forEach((cell) => {
+        if (Math.sqrt(Math.pow(cell.x - centerCell.x, 2) + Math.pow(cell.y - centerCell.y, 2)) < maxDistance) {
+            neighbors.push(cell)
+        }
+    })
+    return neighbors
+}
+
+let done = false
+let repeats = 0
+let ret: Cell[] = []
+function findSameColoredNeighborsOf(cell: Cell): Cell[] {
+    let x = cell.x
+    let y = cell.y
+    let c = cell.rectangle.options.fill
+    let up = getCellByCoords(x, y + 1)
+    let left = getCellByCoords(x - 1, y)
+    let right = getCellByCoords(x + 1, y)
+    let down = getCellByCoords(x, y - 1)
+    let near = [up, left, right, down]
+    repeats++ // TODO: DONT FORGET TO CLEAR IT /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    if (repeats < 5) {
+        near.forEach((dir) => {
+            if (dir && dir.rectangle.options.fill === c && !ret.includes(dir)) {
+                console.log("pushing", dir)
+                ret.push(dir)
+                findSameColoredNeighborsOf(dir)
+            } else {
+            }
+        })
+    }
+    // near.forEach((dir) => {if (dir) {findSameColoredNeighborsOf(dir)}})
+    repeats = 0
+    return ret
+}
+function giveRectangleEventListener(cell: { rectangle: Rectangle, x: number, y: number }) {
+    function toolHandler() {
+        let tool = userSettings.currentToolName
+        let penCol = userSettings.penColor
+        if (tool === "Fill") {
+            findSameColoredNeighborsOf(cell).forEach((neighborCell) => {
+                neighborCell.rectangle.options.fill = penCol
+            })
+        } else if (tool === "Pen") {
+            let neighbors = findNeighborsByDistanceOf(cell, userSettings.penRadius)
+            neighbors.forEach((neighbor) => {
+                neighbor.rectangle.options.fill = penCol
+            })
+        } else if (tool === "Erase") {
+            let neighbors = findNeighborsByDistanceOf(cell, userSettings.penRadius)
+            neighbors.forEach((neighbor) => {
+                neighbor.rectangle.options.fill = userSettings.gridBackgroundColor
+            })
+        }
+    }
+    cell.rectangle.on("hover", () => {
+        if (mouseIsDown)
+            toolHandler()
+    })
+    cell.rectangle.on("mousedown", () => {
+        toolHandler()
+    })
+}
 function createDrawingArea(
     rows: number | undefined = userSettings.rows,
     columns: number | undefined = userSettings.columns,
     backgroundColor: string | undefined = userSettings.gridBackgroundColor,
     gridLineColor: string | undefined = userSettings.gridLineColor,
 ) {
+    let p = el("pencilCanvas")
+    p.remove()
+    // if (p.childNodes.length > 1)
+    let a = document.createElement("div")
+    a.id = "pencilCanvas"
+    document.body.appendChild(a)
+    scene = createScene()
+    scene.startLoop()
+    // scene.clear()
+    // p.childNodes.forEach((node) => {node.remove()})
     let cellWidth = scene.width / rows!
     let cellHeight = scene.height / columns!
     for (let i = 0; i < rows!; i++) {
         for (let j = 0; j < columns!; j++) {
             let r = new Rectangle([cellWidth * i, cellHeight * j], cellWidth - 1, cellHeight - 1, { fill: backgroundColor })
-            r.on("hover", () => { if (mouseIsDown) { r.options.fill = userSettings.penColor } })
-            r.on("mousedown", () => { if (mouseIsDown) { r.options.fill = userSettings.penColor } })
             scene.add(r)
-            cells.push(r)
+            let cellObj = { rectangle: r, x: i, y: j }
+            giveRectangleEventListener(cellObj)
+            cells.push(cellObj)
         }
     }
 }
-type settings = {
-    rows?: number,
-    columns?: number,
-    penColor?: string,
-    gridLineColor?: string,
-    gridBackgroundColor?: string,
-    currentTool: string
+
+// function fillTool() {
+//     window.onclick = () => { }
+// }
+el("about-section").onclick = () => {
+    let e = el("about-section-2")
+    e.style.display === "none" ? e.style.display = "block" : e.style.display = "none"
+}
+class Settings {
+    constructor(
+        public rows: number = 30, // note that it does not output in pixels... Is there a way to change that??
+        public columns: number = 30,
+        public penColor: string = "#aabbff",
+        public gridLineColor: string = "#000000",
+        public gridBackgroundColor: string = "darkslategray",
+        public currentToolName: string = "Pen",
+        public penRadius: number = 2
+    ) { }
 }
 
-function fillTool() {
-    window.onclick = () => {}
-}
 
-function loadSettings(locallySavedSettings?: settings): settings {
-    let defaultSettings = {
-        rows: 30,
-        columns: 30,
-        penColor: penColorHEX,
-        gridLineColor: "#000000",
-        gridBackgroundColor: "darkslategray",
-        currentTool: "Pen"
-    }
+function loadSettings(locallySavedSettings?: Settings): Settings {
+    let defaultSettings = new Settings()
     let settings = defaultSettings
     if (locallySavedSettings !== undefined) {
         Object.assign(settings, locallySavedSettings)
     }
     return settings
 }
-let penColorHEX = "#ffbfaa"
-let userSettings = loadSettings()
-let mouseIsDown
-let cells: Rectangle[] = []
-window.addEventListener("mousedown", () => { mouseIsDown = true })
-window.addEventListener("mouseup", () => { mouseIsDown = false })
 function createToolbarEventListeners() {
     // color input element for changing pen color
     let colin = el("color-input") as HTMLInputElement
-    colin.value = penColorHEX
+    colin.value = userSettings.penColor!
     colin.addEventListener("change", (e) => {
         userSettings.penColor = colin.value
     })
+
     // save button
     el("save-button")!.onclick = () => {
         let imageData = document.querySelector("canvas")!.toDataURL()
@@ -73,10 +165,62 @@ function createToolbarEventListeners() {
         invLink.click();
         document.body.removeChild(invLink);
     }
-    // tool button
-    el("current-tool-button")!.innerText = "Current tool: " + userSettings.currentTool
+
+    //clear button
+    let tb = el("clear-screen-button")
+    tb.onclick = () => {
+        createDrawingArea()
+    }
+
+    // tool buttons
+    function toolButton(toolButtonID: string, toolName: string, backgroundColor: string) {
+        let tb = el(toolButtonID) as HTMLButtonElement
+        tb.onclick = () => {
+            toolButtons.forEach((button) => { button.style.backgroundColor = "white" })
+            userSettings.currentToolName = toolName
+            tb.style.backgroundColor = backgroundColor
+            toolButtons.push(tb)
+        }
+    }
+    let toolButtons: HTMLButtonElement[] = []
+    toolButton("fill-tool-button", "Fill", "orange")
+    toolButton("pen-tool-button", "Pen", "orange")
+    toolButton("erase-tool-button", "Erase", "orange")
+
+
+    // sliders
+    createSlider("row-slider", "row-slider-label", "rows", "Rows", createDrawingArea)
+    createSlider("column-slider", "column-slider-label", "columns", "Columns", createDrawingArea)
+    createSlider("pen-radius-slider", "pen-radius-slider-label", "penRadius", "Radius", () => {})
+    function createSlider(sliderID: string, sliderLabelID: string, controlledUserSetting: string, visibleSettingName: string, also: Function) {
+        let sliderWidth = 100
+        let sliderEl = el(sliderID)
+        let sliderLabel = el(sliderLabelID)
+        // @ts-expect-error TODO: Why does it think it doesn't exist even though I used a "!"?
+        sliderEl.value = userSettings[controlledUserSetting]!
+        sliderEl.style.width = sliderWidth + "px"
+        function updateSlider() {
+            // @ts-expect-error TODO: Why does it think it doesn't exist even though I used a "!"?
+            let val = sliderEl.value
+            sliderLabel.innerText = visibleSettingName + ": " + val
+            userSettings[controlledUserSetting] = val
+        }
+        updateSlider()
+        also()
+        sliderEl.onchange = (e) => { updateSlider(); also() }
+    }
+
 }
+function createScene() {
+    let scene = new Scene(el("pencilCanvas"), { fill: userSettings.gridLineColor })
+    return scene
+}
+let userSettings = loadSettings()
+let mouseIsDown
+let cells: Cell[] = []
+let scene = createScene()
 createToolbarEventListeners()
-let scene = new Scene(el("pencilCanvas"), { fill: userSettings.gridLineColor })
+window.addEventListener("mousedown", () => { mouseIsDown = true })
+window.addEventListener("mouseup", () => { mouseIsDown = false })
 scene.startLoop()
-createDrawingArea()
+// createDrawingArea()
